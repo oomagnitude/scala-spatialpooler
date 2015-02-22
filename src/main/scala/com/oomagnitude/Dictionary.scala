2616b0002c3d5e50621935dc18b845301520165d
yakticus <julie@yakticus.com>
2015-02-21 21:41:41 -0800

    completed first word inference demo

--- src/main/scala/com/oomagnitude/Dictionary.scala
+++ src/main/scala/com/oomagnitude/Dictionary.scala
@@ -11,6 +11,13 @@ object Dictionary {
     .resolve("resources")
     .resolve("google-10000-english.txt").toAbsolutePath.toString
 
-  val Words = Source.fromFile(DictionaryPath).getLines().toIterable
+  // Take the first 1,000 most common English words
+  val Words = Source.fromFile(DictionaryPath).getLines().take(1000).toIterable
+
+  // For each character, the set of all words the character appears in
+  val ReverseIndex = Words
+    .flatMap(w => w.toLowerCase.toCharArray.toIterable.map(c => (c, w)))
+    .groupBy(_._1)
+    .map(kv => kv._1 -> kv._2.map(_._2).toSet)
 
 }
--- src/main/scala/com/oomagnitude/package.scala
+++ src/main/scala/com/oomagnitude/package.scala
@@ -1,5 +1,46 @@
 package com
 
+import com.oomagnitude.geometry.{Coordinate, Geometry}
+import com.oomagnitude.layer.{GlobalInhibition, Layer}
+
+import scala.util.Random
+
 package object oomagnitude {
   type PermanenceFunction = (Double, Boolean) => Double
+
+  val SensorGeometry = Geometry(10,10)
+  val LayerGeometry = Geometry(32,32)
+  val InitialModel = Model(Layer.withRandomConnections(LayerGeometry, SensorGeometry, connectionProbability = 0.20),
+    new GlobalInhibition(maxWinners = 5), List.empty)
+  val random = new Random()
+  val LetterEncodings = ('a' to 'z').zip(random.shuffle(SensorGeometry.coordinates)).toMap
+  val CoordinateToLetter = LetterEncodings.map(_.swap)
+
+  def train: Model = {
+    var model = InitialModel
+    for {
+      i <- 1 to 20
+      word <- Dictionary.Words
+    } {
+      val input = sdrForWord(word)
+      model = model.processInput(input)
+    }
+    model
+  }
+
+  private def sdrForWord(word: String): Set[Coordinate] = {
+    val uniqueChars = word.toLowerCase.toCharArray.toSet
+    uniqueChars.collect {case char if LetterEncodings.contains(char) => LetterEncodings(char)}
+  }
+
+  def inferWord(word: String, model: Model) = {
+    val newModel = model.processInput(sdrForWord(word))
+    val allChars = newModel.winners.flatMap(c => charsForDendrite(c, newModel)).toSet
+    val words = allChars.map(Dictionary.ReverseIndex).reduce((a, b) => a.intersect(b))
+    words.foreach(println)
+  }
+
+  private def charsForDendrite(dendriteAddress: Coordinate, model: Model): Iterable[Char] = {
+    model.layer.dendrites(dendriteAddress).permanentSynapses.keys.map(CoordinateToLetter)
+  }
 }
